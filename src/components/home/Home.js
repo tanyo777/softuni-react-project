358658e04e0c560566fe17b0049fb209afdcf3f1
tanikolo <tanyo.nikolo00@gmail.com>
2022-08-21 11:03:04 +0300

    login button on home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,8 @@
 import styles from "./Home.module.css";
 import Navigation from "../navbars/Navbar";
+import { Button } from "@mui/material";
+import { Send } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,6 +10,11 @@ const Home = () => {
       <Navigation />
       <main className={styles.main}>
         <h1>Welcome to Carify</h1>
+          <Button variant="contained" endIcon={<Send />}>
+            <Link to="/login" className={styles.link}>
+              Login
+            </Link>
+          </Button>
       </main>
     </>
   );
